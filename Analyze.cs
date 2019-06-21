0042aefcb8b7e51c75cba867f6f467e5140f6d73
ploychananya <ploy_youngmodulus@hotmail.com>
2019-06-21 17:08:08 +0300

    Can get all nessesary INFO NOW = finish part 1

--- Analyze.cs
+++ Analyze.cs
@@ -13,167 +13,42 @@ namespace Qualıty_Checker
 {
     public partial class Analyze 
     {
-        //private MFileClass myfile;
         private double rate;
         private string name;
         string strFormat;
         M_AUD_PROPS audProps;
-        //MFReader myReader = new MFReader();
+
         public void Method(string filePath, string quality)
         {
+            Lib lib = new Lib();
             //Playlist
             int nIndex = -1;
             MPlaylistClass myPlaylist = new MPlaylistClass();
             
             myPlaylist.PreviewWindowSet("", 10);
             myPlaylist.PreviewEnable("", 1, 1);
-            //myPlaylist.ObjectStart(new object());
+
             MItem pFile;
             myPlaylist.PlaylistAdd(null, filePath, "", ref nIndex, out pFile);
 
-            /*
-            MPlaylistClass myPlaylist = new MPlaylistClass();
-            myPlaylist.PreviewWindowSet("", 10);
-            myPlaylist.PreviewEnable("", 1, 1);
-            myPlaylist.ObjectStart(new object());
-            int nIndex = -1;
-            MItem pFile;
-            myPlaylist.PlaylistAdd(null, filePath, "", ref nIndex, out pFile);
-            myPlaylist.FilePlayStart();
-            int nCount = 2;
-            //((IMProps)pFile).PropsGetCount("info", out nCount);
-
-            for (int i = 0; i < nCount; i++)
-            {
-                string myName;
-                string myValue;
-                int myNode;
-                ((IMProps)pFile).PropsGetByIndex("info", i, out myName, out myValue, out myNode);
-
-                Console.WriteLine(myName+" "+myValue+ " "+ myNode);
-                ((IMProps)pFile).PropsRemove(myName);
-            }
-            */
-
-
-
-            
-
-
-            //string myVal;
-            //((IMProps)pFile).PropsGet("object", out myVal);
-            //Console.WriteLine(myVal);
-            
-            //Playlist
-
-
-            //MFILECLASS
-            //MFileClass file = new MFileClass();
-            //IMAudioTrack track;
-            //Create new virtual track as full clone of 0 track
-            //file.AudioTrackAdd(0, "", "NewTrack", out track);
-            //Create new virtual track as clone of 1,2,4 channels of 2 track
-            //file.AudioTrackAdd(2, "1,2,4", "NewTrack", out track);
-
-
-
-            //MITEMCLASS
-
-
-            //myPlaylist.FilePlayStart();
-            //yPlaylist.FormatAudioGet(eMFormatType.eMFT_Output, out audProps, out nIndex, out strFormat);
-
-            //int myIndex = -1;
-
-            //MItem myTargetItem;
-            //myPlaylist.PlaylistCommandAdd("mute", "true", myItem, ref myIndex, out myTargetItem);
-
-            //MItem myTargetItem;
-            //myPlaylist.PlaylistCommandAdd("mute", "true", myItem, ref nIndex, out myTargetItem);
-
-            //int nIndex;
-            //string strFormat;
-            //M_AUD_PROPS audProps;
-            //MPlaylistClass myPlaylist = new MPlaylistClass();
-
-            //audProps.nSamplesPerSec = 48000;
-            //myPlaylist.FormatAudioSet(eMFormatType.eMFT_Convert, ref audProps);
-            //filePath = @"/c/Users/ployrue/Desktop";
             //string extraProps = "video_track=-1";
+            MFileClass myFile = new MFileClass();
+            myFile.FileNameSet(filePath, "");
+            myFile.FilePlayStart(); //it has to play for respresent the statistic.
 
-            //MItemClass myFile = new MItemClass();
-            //myFile.FileNameSet(filePath, extraProps);
-
-
-            //myFile.PropsSet("active_frc", "0");
-            //myFile.PropsSet("on_frame.sync", "true");
-            //myFile.PropsSet("file::video_track", "-1");
-            //myFile.PropsSet("loop", "false");
-
-            //yFile.OnEventSafe += m_objFile_OnEventSafe;
-            //myFile.OnFrameSafe -= m_objFileScan_OnFrameSafe;
-
-            //myFile.PropsSet("rate_control", "false");
-            //myFile.FileNameSet(filePath, "");
-
-
-            //.ObjectStart(m_objPlaylist);
-            //myFile.ObjectStart(myFile);
-            //Console.WriteLine("between frame: 100-110");
-
-
-            //myFile.FileNameSet(filePath, extraProps);
-
-            // pull vidio file to variable;
             if (quality == "-solution")
             {
-                //string file;
-                //myFile.FileNameGet(out file);
-
-                //int nIndex;
-                //string strFormat;
-                //M_VID_PROPS vidProps;
-                //myFile.FormatVideoGet(eMFormatType.eMFT_Output, out vidProps, out nIndex, out strFormat);
-                //Console.WriteLine((vidProps.nAspectX).ToString() + " * " + (vidProps.nAspectY).ToString() + "\n"+" ScaleType: " + (vidProps.eScaleType).ToString() + "  bitrate: "+ (vidProps.dblRate).ToString());
-                //Console.WriteLine(nIndex.ToString());
-                //Console.WriteLine(strFormat.ToString());
-                //Console.WriteLine((eMFormatType.eMFT_Output).ToString());
-
-
-                //work
-                //////////////////////////////////////////////////////////////////////////////
-                /*MPlaylistClass myPlaylist = new MPlaylistClass();
-                //myPlaylist.PreviewWindowSet("", panelPreview.Handle.ToInt32());
-                myPlaylist.PreviewWindowSet("", 2);
-                myPlaylist.PreviewEnable("", 1, 1);
-                myPlaylist.ObjectStart(new object());
-                //Insert clip at the end of playlist
-                int nIndex = -1;
-                MItem pFile;
-                string strPath = @"c:/Users/ployrue/Desktop/123.mp3";
-                myPlaylist.PlaylistAdd(null, strPath, "", ref nIndex, out pFile);
-                myPlaylist.FilePlayStart();
-                */
-                //////////////////////////////////////////////////////////////////////////////
-
-                //MPlaylist myPlaylist = new MPlaylist();
-                //myPlaylist.PreviewWindowSet("", panelPreview.Handle.ToInt32());
-                //myPlaylist.PreviewEnable("", 1, 1);
-                //myPlaylist.ObjectStart(new object());
-                //Insert clip at the end of playlist
-                //int nIndex = -1;
-                //MItem pFile;
-                //string strPath = @"C:/Users/ployrue/Desktop/123.mp3";
-                //myPlaylist.PlaylistAdd(null, strPath, "", ref nIndex, out pFile);
-                //myPlaylist.FilePlayStart();
-                //myPlaylist.PlaylistBackgroundGet();
-                //myFile.FormatVideoGet();
-
-            }else if (quality == "-info")
+            
+            }else if (quality == "-info")//Only Object Property
             {
+                //string myVal;
+                //((IMProps)pFile).PropsGet("file::info::subtitle.0::codec_name", out myVal);
+                //Console.WriteLine(myVal);
 
+                /*
+                Console.WriteLine("     info IMProps: ");
                 int nCount;
-                ((IMProps)pFile).PropsGetCount("", out nCount); //Only Object Property
+                ((IMProps)pFile).PropsGetCount("", out nCount); 
                 for (int i = 0; i < nCount; i++)
                 {
                     string myName;
@@ -184,17 +59,25 @@ namespace Qualıty_Checker
                     //Console.WriteLine(myValue);
                     //((IMProps)myItem).PropsRemove(myName);
                 }
+
+
+                Console.WriteLine("     info MFILE not specific: ");
+                lib.GetFileInfo(myFile, "");
+                */
+                lib.GetFileInfo(myFile, "file::info");
+                /*
+                Console.WriteLine("     file MFILE: ");
+                string myVal;
+                (myFile as IMProps).PropsGet("object::info::codec", out myVal);
+                Console.WriteLine(myVal);
+                */
             }
             else if (quality == "-codec")
             {
-                //find solution 
-                //show solution 
                 Console.WriteLine("Codec: h263, h264");
             }
             else if (quality == "-freez")
             {
-                //find solution 
-                //show solution 
                 Console.WriteLine("between frame: 100-110");
             }
             else if (quality == "-audChanel")
@@ -218,22 +101,6 @@ namespace Qualıty_Checker
             }
             else if (quality == "-play")
             {
-                //MPlaylistClass myPlaylist = new MPlaylistClass();
-                //myPlaylist.PreviewWindowSet("", panelPreview.Handle.ToInt32());
-
-                //myPlaylist.PreviewWindowSet("", 10);
-                //myPlaylist.PreviewEnable("", 1, 1);
-                //myPlaylist.ObjectStart(new object());
-
-                //Insert clip at the end of playlist
-
-                //w int nIndex = -1;
-                //w MItem pFile;
-
-                //string strPath = @"c:/Users/ployrue/Desktop/123.mp3";
-
-                //w myPlaylist.PlaylistAdd(null, filePath, "", ref nIndex, out pFile);
-
                 myPlaylist.FilePlayStart();
             } else if (quality == "-name")
             {
@@ -242,32 +109,15 @@ namespace Qualıty_Checker
                 Console.WriteLine("File's name : " + FileName.ToString());
             } else if (quality == "-format")
             {
-                //int nIndex;
-                //string strFormat;
-                //M_AUD_PROPS audProps;
-                //MPlaylistClass myPlaylist = new MPlaylistClass();
-                //eMFormatType autype;
                 myPlaylist.FormatAudioGet(eMFormatType.eMFT_Output, out audProps, out nIndex, out strFormat);
-
-                //audProps.nSamplesPerSec = 48000;
-                //myPlaylist.FormatAudioSet(eMFormatType.eMFT_Convert, ref audProps);
                 Console.WriteLine("Format: " + strFormat);
-                //Console.WriteLine("Format2: " + myItem.FileRateGet()
             } else if (quality == "-sRate")
             {
-                //string strFormat;
-                //M_AUD_PROPS audProps;
-                //MPlaylistClass myPlaylist = new MPlaylistClass();
-
-                //myPlaylist.FormatAudioGet(eMFormatType.eMFT_Output, out audProps, out nIndex, out strFormat);
-
-                //audProps.nSamplesPerSec = 48000;
-                //myPlaylist.FormatAudioSet(eMFormatType.eMFT_Convert, ref audProps);
-                
                 pFile.FileRateGet(out rate);
                 Console.WriteLine("Rate : " + audProps.nSamplesPerSec + "X");
                 Console.WriteLine("Sample Rate : " + audProps.nSamplesPerSec);
             }
             }
     }
+    
 }
---
+++ Lib.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPLATFORMLib;
+
+namespace Qualıty_Checker
+{
+    public class Lib
+    {
+        public void GetFileInfo(MFileClass m_objMFReader, string propertyNode)
+        {
+            //string fileInfo = "";
+            int nCount = 0;
+            try
+            {
+                // get a number of properties
+                m_objMFReader.PropsGetCount(propertyNode, out nCount);
+            }
+            catch (Exception) { }
+            for (int i = 0; i < nCount; i++)
+            {
+                string sName;
+                string sValue;
+                int bNode = 0;
+                m_objMFReader.PropsGetByIndex(propertyNode, i, out sName, out sValue, out bNode);
+                // bNode flag indicates whether there are internal properties
+                // to collect a full node name we should separated it with "::", e.g. "info::video.0"
+                string sRelName = propertyNode.Length > 0 ? propertyNode + "::" + sName : sName;
+                if (bNode != 0)
+                {
+                    GetFileInfo(m_objMFReader,sRelName); // call the method recursively in case of sub-nodes
+                }
+                else
+                {
+                    Console.WriteLine(sRelName + " = " + sValue + Environment.NewLine);
+                    //fileInfo += sRelName + " = " + sValue + Environment.NewLine;
+                }
+
+            }
+            //return fileInfo;
+        }
+    }
+}
